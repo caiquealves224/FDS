2cbd1e5b144403197c97f889e3986468468ff1d3
caiquealves224 <37445916+caiquealves224@users.noreply.github.com>
2018-09-02 18:13:28 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,46 @@
+let empresas = {
+    "1":["Saint","admin"],
+    "2":["Mit","321"],
+    "3":["Microsoft","321"],
+    "4":["IBM","321"]
+}
+let botao =document.querySelector("#login");
+
+botao.addEventListener('click', function(){
+
+    login = prompt("Digite seu nome");
+    senha = prompt("Digite sua senha");
+    
+    if (usuarios[1][0] == login && usuarios[1][2] == senha) { alert('Welcome'); } else { alert("Acesso Negado"); }
+})
+
+let botao2=document.querySelector(".botaoenviar");
+
+    
+    botao2.addEventListener('click', function (){
+    alert("Mensagem Enviada");
+    });
+
+// let usuarios = {
+//     "1":["caique","123"],
+//     "2":["henrique","321"]
+// }
+
+
+
+
+// vezes=prompt("Quantas vezes dejesa executar essa tarefa?");
+// let i=1;
+// while(i<=vezes)
+// {
+//     idade=prompt("Qual a sua idade?");
+//     salario=prompt("Quanto ganhas?");
+//     empres=prompt("quanto queres de empréstimo?");
+
+//     if(idade>21 && idade <55 && empres<10*salario){
+//         alert("Empréstimo Aprovado");
+//     }else{
+//         alert("Empréstimo Reprovado");
+//     }
+//     i++
+// }
